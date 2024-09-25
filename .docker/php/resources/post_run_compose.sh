24c42c402dcadb21f03665528213bbc5b426d2a8
Will Woodlief <willwoodlief@gmail.com>
2024-09-25 14:57:48 -0500

    first commit with empty laravel project run in docker

---
+++ .docker/php/resources/post_run_compose.sh
@@ -0,0 +1,24 @@
+#uploads for uploading images and docs
+if [[ ! -e /var/www/html/storage/app/uploads ]]; then
+    mkdir -p /var/www/html/storage/app/uploads
+fi
+
+chmod 755 /var/www/html/storage/app/uploads
+
+if [[ ! -e /var/www/html/public/uploads ]]; then
+    ln -s /var/www/html/storage/app/uploads /var/www/html/public
+fi
+
+# when docker-compose starts, some directors may not be readable to the php or web, Run this script in the container terminal
+chown -R root:www-data /var/www/html/storage/
+chown -R root:www-data /var/www/html/public/storage
+chown -R root:www-data /var/www/html/bootstrap/cache
+
+chmod -R g+rw /var/www/html/storage/
+chmod -R g+rw /var/www/html/public/storage
+chmod -R g+rw /var/www/html/bootstrap/cache
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# hexbatch-core-inspector
+
+Play with the core in a browser.
+
+This is a development aid to work with the core using graphics, gui and visual feedback
